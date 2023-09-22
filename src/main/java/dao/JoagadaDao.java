f72d58bee71a2e218b3dcba2f638572a5c2a515e
Matthews R. Sampaio <85257122+matthewsrsampaio@users.noreply.github.com>
2023-09-22 14:39:45 -0300

    first commit

---
+++ src/main/java/dao/JoagadaDao.java
@@ -0,0 +1,78 @@
+package dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import entity.Jogada;
+import util.Jpa;
+
+public class JoagadaDao {
+	
+	public static void salvar(Jogada jogada) throws Exception {
+	    EntityManager em = Jpa.criarEntityManager();
+	    try {
+		    em.getTransaction().begin();
+		    em.persist(jogada);
+		    em.getTransaction().commit();
+	    } catch (Exception e) {
+		    throw e;
+	    } finally {
+		    em.close();
+	    }
+	}
+	
+	public static void editar(Jogada jogada) {
+		EntityManager em = Jpa.criarEntityManager();
+		try {
+			em.getTransaction().begin();
+			em.merge(jogada);
+			em.getTransaction().commit();
+		} catch(Exception e) {
+			throw e;
+		} finally {
+			em.close();
+		}
+	}
+	
+	public static List<Jogada> buscarTodos() throws Exception {
+		EntityManager em = Jpa.criarEntityManager();
+		try {
+			Query query = em.createQuery("select c from Comida c");
+			List<Jogada> lista = query.getResultList();
+			return lista;
+		}catch(Exception e) {
+			throw e;
+		}finally {
+			em.close();
+		}
+	}
+
+	public static void deletar(Jogada jogada) throws Exception {
+		EntityManager em = Jpa.criarEntityManager();
+		try {
+			em.getTransaction().begin();
+			jogada = em.find(Jogada.class, jogada.getId());
+			em.remove(jogada);
+			em.getTransaction().commit();
+		}catch(Exception e) {
+			throw e;
+		}finally {
+			em.close();
+		}
+	}
+	
+	public static Jogada buscarPorId(Integer id) throws Exception{
+		EntityManager em = Jpa.criarEntityManager();
+		try {
+			Jogada jogada = em.find(Jogada.class, id);
+			return jogada;
+		}catch(Exception e) {
+			throw e;
+		}finally {
+			em.close();
+		}
+	}
+
+}
